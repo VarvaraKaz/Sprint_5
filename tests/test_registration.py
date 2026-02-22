e9b21ed84d4043173a512163b938078a132f962b
VarvaraKaz <cfvarvara@gmail.com>
2026-02-22 12:50:55 +0100

    created data.py file for test data and updated the code

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,13 +1,15 @@
 from pages.registration_page import RegistrationPage
 import random
+from generators import Generate_Email
+from data import TEST_NAME, TEST_PASSWORD
 
 def test_registration_success(driver):
     registration_page = RegistrationPage(driver)
     registration_page.open_registration_page()
 
-    name = 'TestName'
-    email = registration_page.generate_email()
-    password = '123456'
+    name = TEST_NAME
+    email = Generate_Email()
+    password = TEST_PASSWORD
     registration_page.register_user(name, email, password)
     assert registration_page.is_login_button_displayed()
 
@@ -15,8 +17,8 @@ def test_error_for_password_field(driver):
     page = RegistrationPage(driver)
     page.open_registration_page()
 
-    name = 'TestName'
-    email = page.generate_email()
+    name = TEST_NAME
+    email = Generate_Email()
     password = f'{random.randint(1,99999)}'
     page.register_user(name, email, password)
     error_text = page.get_password_error()
