7225364cea350a5a6885ccc1f38c38ee638a6658
VarvaraKaz <cfvarvara@gmail.com>
2026-02-22 13:02:40 +0100

    updated names for locators, moved generate email method to a separate file - generators.py

--- pages/registration_page.py
+++ pages/registration_page.py
@@ -9,19 +9,19 @@ class RegistrationPage(BasePage):
         self.open(REGISTRATION_URL)
 
     def enter_name(self, name):
-        self.send_keys(RegistrationPageLocators.Name_Input, name)
+        self.send_keys(RegistrationPageLocators.NAME_INPUT, name)
 
     def enter_email(self, email):
-        self.send_keys(RegistrationPageLocators.Email_Input, email)
+        self.send_keys(RegistrationPageLocators.EMAIL_INPUT, email)
 
     def enter_password(self, password):
-        self.send_keys(RegistrationPageLocators.Password_Input, password)
+        self.send_keys(RegistrationPageLocators.PASSWORD_INPUT, password)
 
     def click_register(self):
-        self.click(RegistrationPageLocators.Register_Button)
+        self.click(RegistrationPageLocators.REGISTER_BUTTON)
 
     def get_password_error(self):
-        return self.get_text(RegistrationPageLocators.Password_Error)
+        return self.get_text(RegistrationPageLocators.PASSWORD_ERROR)
     
     def register_user(self, name, email, password):
         self.enter_name(name)
@@ -30,14 +30,10 @@ class RegistrationPage(BasePage):
         self.click_register()
 
     def is_login_button_displayed(self):
-        return self.find_element(LogInPageLocators.Login_button_Login_Page).is_displayed()
+        return self.find_element(LogInPageLocators.LOGIN_BUTTON_LOGIN_PAGE).is_displayed()
     
     def click_login_button(self):
-        self.click(RegistrationPageLocators.Login_Button_Registration_Page)
+        self.click(RegistrationPageLocators.LOGIN_BUTTON_REGISTRATION_PAGE)
 
     def click_personal_account_button(self):
-        self.click(MainPageLocators.Personal_Account_Button)
-
-    @staticmethod
-    def generate_email():
-        return f'varvara_kazarosian_40_{random.randint(100,999)}@gmail.com'
+        self.click(MainPageLocators.PERSONAL_ACCOUNT_BUTTON)
