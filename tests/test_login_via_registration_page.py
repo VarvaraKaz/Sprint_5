ec43850d2fd65eb9f44fa331539caa274d867aa2
VarvaraKaz <cfvarvara@gmail.com>
2026-02-23 11:30:50 +0100

    created classes for tests, created one method for logging in in login_page.py and updated the code for tests

--- tests/test_login_via_registration_page.py
+++ tests/test_login_via_registration_page.py
@@ -1,25 +1,21 @@
 from pages.registration_page import RegistrationPage
 from pages.login_page import LoginPage
-from conftest import LOGIN_CREDENTIALS
 
-def test_login_via_registration_page(driver):
-    registration_page = RegistrationPage(driver)
-    login_page = LoginPage(driver)
+class TestLoginViaRegistrationAndPersonalAccountPages:
+    def test_login_via_registration_page(self, driver):
+        registration_page = RegistrationPage(driver)
+        login_page = LoginPage(driver)
 
-    registration_page.open_registration_page()
-    registration_page.click_login_button()
-    login_page.wait_for_login_page()
-    login_page.login(LOGIN_CREDENTIALS["email"], LOGIN_CREDENTIALS["password"])
-    login_page.click_login_button()
-    assert login_page.is_login_successful()
+        registration_page.open_registration_page()
+        registration_page.click_login_button()
+        login_page.login_as_valid_user()
+        assert login_page.is_login_successful()
 
-def test_login_via_personal_account_button_on_registration_page(driver):
-    registration_page = RegistrationPage(driver)
-    login_page = LoginPage(driver)
+    def test_login_via_personal_account_button_on_registration_page(self, driver):
+        registration_page = RegistrationPage(driver)
+        login_page = LoginPage(driver)
 
-    registration_page.open_registration_page()
-    registration_page.click_personal_account_button()
-    login_page.wait_for_login_page()
-    login_page.login(LOGIN_CREDENTIALS["email"], LOGIN_CREDENTIALS["password"])
-    login_page.click_login_button()
-    assert login_page.is_login_successful()
+        registration_page.open_registration_page()
+        registration_page.click_personal_account_button()
+        login_page.login_as_valid_user()
+        assert login_page.is_login_successful()
