8f51501f41cca4c95b6d0ec4f7e1bc5c7ebaefd9
VarvaraKaz <cfvarvara@gmail.com>
2026-02-23 11:31:25 +0100

    created classes for tests, created one method for logging in in login_page.py and updated the code for tests

--- tests/test_main_page.py
+++ tests/test_main_page.py
@@ -3,42 +3,42 @@ from pages.login_page import LoginPage
 from conftest import LOGIN_CREDENTIALS
 
 
-def test_login_via_main_page(driver):
-    page = MainPage(driver)
-    login_page = LoginPage(driver)
+class TestLoginViaMainPage:
+    def test_login_via_main_page(self, driver):
+        page = MainPage(driver)
+        login_page = LoginPage(driver)
 
-    page.open_main_page()
-    page.click_login_button_on_main_page()
-    login_page.wait_for_login_page()
-    login_page.login(LOGIN_CREDENTIALS["email"], LOGIN_CREDENTIALS["password"])
-    login_page.click_login_button()
-    assert login_page.is_login_successful()
+        page.open_main_page()
+        page.click_login_button_on_main_page()
+        login_page.login_as_valid_user()
+        assert login_page.is_login_successful()
 
-def test_sauces_tab_is_opened_when_clicked(driver):
-    page = MainPage(driver)
+class TestMainPage:
+    def test_sauces_tab_is_opened_when_clicked(self, driver):
+        page = MainPage(driver)
 
-    page.open_main_page()
-    page.click_sauces_tab()
-    assert page.is_sauces_tab_active()
+        page.open_main_page()
+        page.click_sauces_tab()
+        assert page.is_sauces_tab_active()
 
-def test_bulki_tab_is_opened_when_clicked(driver):
-    page = MainPage(driver)
+    def test_bulki_tab_is_opened_when_clicked(self, driver):
+        page = MainPage(driver)
 
-    page.open_main_page()
-    page.click_nachinki_tab()
-    page.click_bulki_tab()
-    assert page.is_bulki_tab_active()
+        page.open_main_page()
+        page.click_nachinki_tab()
+        page.click_bulki_tab()
+        assert page.is_bulki_tab_active()
 
-def test_nachinki_tab_is_opened_when_clicked(driver):
-    page = MainPage(driver)
+    def test_nachinki_tab_is_opened_when_clicked(self, driver):
+        page = MainPage(driver)
 
-    page.open_main_page()
-    page.click_nachinki_tab()
-    assert page.is_nachinki_tab_active()
+        page.open_main_page()
+        page.click_nachinki_tab()
+        assert page.is_nachinki_tab_active()
 
-def test_open_personal_account_from_main_page_when_logged_in(login_successful):
-    page = MainPage(login_successful.driver)
-    assert login_successful.is_login_successful()
+    def test_open_personal_account_from_main_page_when_logged_in(self, login_successful):
+        page = MainPage(login_successful.driver)
+        assert login_successful.is_login_successful()
 
-    page.click_personal_account()
-    assert page.is_personal_account_opened()
+        page.click_personal_account()
+        assert page.is_personal_account_opened()
