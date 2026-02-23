990aa153e243f9d08210067589341ef30e364d78
VarvaraKaz <cfvarvara@gmail.com>
2026-02-23 11:31:42 +0100

    created classes for tests

--- tests/test_personal_account_page.py
+++ tests/test_personal_account_page.py
@@ -1,31 +1,31 @@
 from pages.personal_account_page import PersonalAccountPage
 
-def test_open_constructor_from_personal_account_page(login_successful):
-    main_page = login_successful
-    assert login_successful.is_login_successful()
-    personal_account_page = PersonalAccountPage(main_page.driver)
-    
-    personal_account_page.open_personal_account()
-    personal_account_page.click_constructor()
-    assert personal_account_page.is_constuctor_visible()
+class TestPersonalAccountPage:
+    def test_open_constructor_from_personal_account_page(self, login_successful):
+        main_page = login_successful
+        assert login_successful.is_login_successful()
+        personal_account_page = PersonalAccountPage(main_page.driver)
+        
+        personal_account_page.open_personal_account()
+        personal_account_page.click_constructor()
+        assert personal_account_page.is_constuctor_visible()
 
-def test_click_logo_from_personal_account_page(login_successful):
-    main_page = login_successful
-    assert login_successful.is_login_successful()
+    def test_click_logo_from_personal_account_page(self, login_successful):
+        main_page = login_successful
+        assert login_successful.is_login_successful()
 
-    personal_account_page = PersonalAccountPage(main_page.driver)
+        personal_account_page = PersonalAccountPage(main_page.driver)
 
-    personal_account_page.open_personal_account()
-    personal_account_page.click_logo()
-    assert personal_account_page.is_constuctor_visible()
+        personal_account_page.open_personal_account()
+        personal_account_page.click_logo()
+        assert personal_account_page.is_constuctor_visible()
 
-def test_logout_from_personal_account_page(login_successful):
-    main_page = login_successful
-    assert login_successful.is_login_successful()
+    def test_logout_from_personal_account_page(self, login_successful):
+        main_page = login_successful
+        assert login_successful.is_login_successful()
 
-    personal_account_page = PersonalAccountPage(main_page.driver)
+        personal_account_page = PersonalAccountPage(main_page.driver)
 
-    personal_account_page.open_personal_account()
-    personal_account_page.click_logout_button()
-    assert personal_account_page.is_logout_successful()
-
+        personal_account_page.open_personal_account()
+        personal_account_page.click_logout_button()
+        assert personal_account_page.is_logout_successful()
